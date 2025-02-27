378e170998675a08ec637c4b11ced641d1ffbf00
unknown <sandra.rivera.o@uniautonoma.edu.co>
2025-02-27 13:26:32 -0500

    Next

---
+++ src/components/descarga.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function descarga() {
+  return (
+    <div>descarga</div>
+  )
+}
+
+export default descarga
---
+++ src/components/misionvision.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function misionvision() { return (
+    <div>misionvision</div> )
+}
+
+export default misionvision
